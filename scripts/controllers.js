292936aa317f057f41e463a971dc22edf549015c
tombuchaillot89@gmail.com <tombuchaillot89@gmail.com>
2016-08-13 00:52:19 -0300

    ctom-1.3
    controllers.js

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-/**
- * Created by tmz on 05/05/16.
- */
+
 var app = angular.module('blueharvest.controllers', [
     'blueharvest.controllers.home',
     'blueharvest.controllers.servicios'
 ]);
-=======
-/**
- * Created by tmz on 05/05/16.
- */
-var app = angular.module('blueharvest.controllers', [
-    'blueharvest.controllers.home',
-    'blueharvest.controllers.servicios'
-]);
->>>>>>> origin/master
